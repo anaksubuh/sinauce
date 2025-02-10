fb6a92183f5f5f49e88ba70e3ee533592bf2ae85
anak subuh <75495151+anaksubuh@users.noreply.github.com>
2025-02-10 17:18:46 +0700

    Add files via upload

---
+++ Modul 1/hello3.cpp
@@ -0,0 +1,31 @@
+/* --------------------------------------------------
+
+	Nama Program : Hello World
+	Nama         : Johanes Septiawan
+	NPM          : 2440501116
+	Tanggal Buat : 10 Februari 2024
+	Deskripsi    : Menampilakan Output Hello World
+
+   -----------------------------------------------*/
+
+#include <iostream>
+
+int main()
+{
+	// Menampilkan Output
+	std::cout << "========= Data Mahasiswa =========" ;
+	std::cout << "|Nama   : Johanes Septiawan      |" ;
+	std::cout << "|Npm    : 2440501116             |" ;
+	std::cout << "|Asal   : Magelang               |" ;
+	std::cout << "|          					   |" ;
+	std::cout << "--------- Mata Kuliah ------------" ;
+	std::cout << "| Kode  | Nama Mtkul  | SKS      |" ;
+	std::cout << "----------------------------------" ;
+	std::cout << "| MK001 | Matematika  | 3        |" ;
+	std::cout << "| MK002 | Bsis Data   | 3        |" ;
+	std::cout << "| MK003 | Data Mining | 3        |" ;
+	std::cout << "----------------------------------" ;
+	std::cout << "Total SKS             | 8        |" ;
+	
+	return 0;
+}
