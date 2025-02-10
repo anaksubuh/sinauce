05445b1bcec48e0d77594961433120bd90401147
anak subuh <75495151+anaksubuh@users.noreply.github.com>
2025-02-10 16:35:30 +0700

    Add files via upload

---
+++ Modul 1/helloworld.cpp
@@ -0,0 +1,19 @@
+/* --------------------------------------------------
+
+	Nama Program : Hello World
+	Nama         : Johanes Septiawan
+	NPM          : 2440501116
+	Tanggal Buat : 10 Februari 2024
+	Deskripsi    : Menampilakan Output Hello World
+
+   -----------------------------------------------*/
+
+#include <iostream>
+
+int main()
+{
+	// Menampilkan Output
+	std::cout << "Hello World!!!";
+
+	return 0;
+}
